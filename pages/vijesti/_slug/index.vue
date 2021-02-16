046e0573f21b7e44dd4e6bcc34b437ddb0ada015
ngazic <ngazic@gmail.com>
2021-02-16 17:01:06 +0100

    added extra pages

---
+++ pages/vijesti/_slug/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>This is vijesti page with parameter {{ this.$route.params.slug }}</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/vijesti/index.vue
+++ pages/vijesti/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Foto</h1>
+    <h1>This is vijesti page</h1>
   </div>
 </template>
 
